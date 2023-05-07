6aced51c25893a22cf24100f0f808120a69d522c
Deven Shah <devenshah1974@gmail.com>
2023-05-08 00:22:48 +0100

    Added database project

---
+++ src/Indiwiz.Trading.Database/Tables/Instrument.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Instrument]
+(
+    [Id] BIGINT NOT NULL IDENTITY(1,1),
+    [ISIN] VARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(200) NOT NULL,
+    [CurrencyName] NVARCHAR(10) NOT NULL,
+    [Ticker] NVARCHAR(10) NOT NULL
+    CONSTRAINT [PK_Instrument] PRIMARY KEY ([Id]),
+    CONSTRAINT [UK_Instrument] UNIQUE ([ISIN])
+)
