941e616be2dabc9361b6a82d0042f242521288ca
Deven Shah <devenshah1974@gmail.com>
2023-10-14 12:13:53 +0100

    Added instrument profit and loss summary

--- src/Indiwiz.Trading.Web/Pages/InstrumentDetails.cshtml.cs
+++ src/Indiwiz.Trading.Web/Pages/InstrumentDetails.cshtml.cs
@@ -10,6 +10,15 @@ public class InstrumentDetailsModel : PageModel
 
     public Instrument Instrument { get; set; } = null!;
 
+    public decimal CurrentPrice { get; set; }
+    public decimal StockBought { get; set; }
+    public decimal TotalBoughtAmount { get; set; }
+    public decimal StockSold { get; set; }
+    public decimal TotalSoldAmount { get; set; }
+    public decimal StockInHand => StockBought - StockSold;
+    public decimal StockInHandAmount => StockInHand * CurrentPrice;
+    public decimal ProfitAndLoss => TotalSoldAmount + StockInHandAmount - TotalBoughtAmount;
+
     public InstrumentDetailsModel(IInstrumentsRepository instrumentsRepository)
     {
         _instrumentsRepository = instrumentsRepository;
@@ -18,5 +27,9 @@ public class InstrumentDetailsModel : PageModel
     public async Task OnGetAsync(long instrumentId)
     {
         Instrument = await _instrumentsRepository.GetInstrumentById(instrumentId);
+        StockBought = Instrument.Orders.Where(o => o.TransactionType == TransactionType.Buy).Sum(o => o.Quantity);
+        TotalBoughtAmount = Instrument.Orders.Where(o => o.TransactionType == TransactionType.Buy).Sum(o => o.AmountInAccountCurrency);
+        StockSold = Instrument.Orders.Where(o => o.TransactionType == TransactionType.Sell).Sum(o => o.Quantity);
+        TotalSoldAmount = Instrument.Orders.Where(o => o.TransactionType == TransactionType.Sell).Sum(o => o.AmountInAccountCurrency);
     }
 }
