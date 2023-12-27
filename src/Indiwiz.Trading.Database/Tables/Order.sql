f09fbf675fa43b60713ceade2dee7bbaae865d76
Deven Shah <devenshah1974@gmail.com>
2023-12-27 14:29:22 +0000

    Added groupid to order table

--- src/Indiwiz.Trading.Database/Tables/Order.sql
+++ src/Indiwiz.Trading.Database/Tables/Order.sql
@@ -9,6 +9,7 @@
     [RateInInstrumentCurrency] DECIMAL(18, 5) NOT NULL,
     [AmountInAccountCurrency] DECIMAL(18, 2) NOT NULL,
     [OrderDate] DATETIME2 NOT NULL,
+    [GroupId] UNIQUEIDENTIFIER NULL,
     CONSTRAINT [PK_Order] PRIMARY KEY ([Id]),
     CONSTRAINT [UK_Order] UNIQUE ([OrderId]),
     CONSTRAINT [FK_Order_Instrument] FOREIGN KEY ([InstrumentId]) REFERENCES [Instrument]([Id]),
